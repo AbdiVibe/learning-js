a6d2d686abe6ee6979fdc9bbaa27becf2315a9c5
AbdiVibe <Abdimalikvibe@gmail.com>
2026-07-09 21:07:37 +0300

    Initial commit

---
+++ 01-variables/script.js
@@ -0,0 +1,40 @@
+example : 1 // Hello World
+    //write a program that prints "Hello World!" to the console
+        console.log("Hello World!");
+
+    //Modify it to print your name instead
+        console.log("AbdiVibe");
+
+    //create a variable called greeting and assign it a greeting message, then print it    
+        let greeting = "Welcome to JavaScript lesson!";
+            console.log(greeting); 
+Exampe : 2  // Variable Practice
+    //Declare a const variable for your Birth year
+        const birthYear = 2002;  
+                 console.log(birthYear); 
+    //Declare a let variable for your age and assign it a value        
+        let age = 23;
+            console.log(age);   
+Example : 3 // Data Types
+    // Create a variable representing: 
+        // Your favorite book (string)
+        let favoriteBook = "Atomic Habits";
+            console.log(favoriteBook);
+        // The year it was published (number)
+        let publishedYear = 2018;
+            console.log(publishedYear);
+        // Whether you have read it (boolean)
+        let hasReadBook = true;
+            console.log(hasReadBook);
+        // Your rating if you've read it, or null if not
+        let rating = 9;
+            console.log(rating);
+        // Use typeof to check the type of these variables
+        console.log(typeof favoriteBook);
+        console.log(typeof publishedYear);
+        console.log(typeof hasReadBook);
+        console.log(typeof rating);
+        // Create a variable but don't assign it a value. check it's value and type
+        let myVariable;
+            console.log(myVariable);
+            console.log(typeof myVariable);
